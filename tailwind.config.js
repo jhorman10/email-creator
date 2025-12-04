7f0fd26e2ffe96e466f375b7cba7bf8a360c01c2
Jhorman Orozco <jsojhor@gmail.com>
2025-12-04 18:16:40 -0500

    chore(theme): extend Tailwind's rose palette with softer pastel colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ export default {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Override Tailwind's default `rose` palette with softer, pastel values
+        rose: {
+          50:  '#fff9fb',
+          100: '#fff1f5',
+          200: '#ffe6ee',
+          300: '#ffd6e6',
+          400: '#ffc6db',
+          500: '#ffb3cf',
+          600: '#ff9fbd',
+          700: '#ff88aa',
+          800: '#ff6f95',
+          900: '#ff527a',
+        }
+      }
+    },
   },
   plugins: [],
 }
